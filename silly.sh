7f1702e0c118bf648561ad5b2edfc542f3198845
ManuP6789 <manuel.pena654576@tufts.edu>
2025-11-12 20:37:04 -0500

    Modify script on install

--- silly.sh
+++ silly.sh
@@ -7,6 +7,27 @@ set -eux  # exit on errors and print each command
 sudo apt update -y
 
 # Install dependencies
-sudo apt install -y clang libelf-dev zlib1g-dev
+sudo apt install -y clang libelf1 libelf-dev zlib1g-dev libssl-dev
+
+# Install optional packages (uncomment if needed)
+# sudo apt install -y binutils-dev libcap-dev llvm gcc make build-essential
+
+cd /local/repository/
+
+git submodule update --init --recursive
+
+# Install bpftool
+cd third_party/bpftool/src
+
+sudo make install
+
+# Generate vmlinux.h for BPF CO:RE
+cd ../../vmlinux/x86/
+
+sudo bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+
+cd ../../../
+
+make
 
 echo "✅ Setup complete: clang and required libraries installed."
