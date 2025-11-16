16718fe6b824baf379229f17a255007500763401
ManuP6789 <manuel.pena654576@tufts.edu>
2025-11-16 11:51:34 -0500

    Specify x86 node

--- profile.py
+++ profile.py
@@ -19,8 +19,7 @@ request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
 node = request.RawPC("node")
-node.architecture = "x86_64"
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+node.hardware_type = "m510"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
